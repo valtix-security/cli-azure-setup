b10cb0d36253ae9c0a937a511160bb3b23aae05a
Ramana Pillalamarri <balpilla@cisco.com>
2025-06-10 16:12:17 +0530

    VAL-19095: VWAN Permissions

--- azure-setup.sh
+++ azure-setup.sh
@@ -103,7 +103,9 @@ cat > /tmp/role.json <<- EOF
       "Microsoft.Compute/diskEncryptionSets/read",
       "Microsoft.Insights/Metrics/Read",
       "Microsoft.Network/locations/serviceTagDetails/read",
-      "Microsoft.Network/locations/serviceTags/read"
+      "Microsoft.Network/locations/serviceTags/read",
+      "Microsoft.Network/virtualHubs/*",
+      "Microsoft.Network/virtualHubs/hubRouteTables/*"
     ],
     "AssignableScopes": [
         "/subscriptions/$sub_id"
